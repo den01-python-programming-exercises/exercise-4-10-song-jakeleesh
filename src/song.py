d859e5be0f92041158d563c1798a9645a99b33fe
Jake Lee <jake.lee@fdmgroup.com>
2021-10-12 14:38:22 +0800

    Attempts solution

---
+++ src/song.py
@@ -0,0 +1,10 @@
+class Song:
+    def __init__(self, name, length):
+        self.song_name = name
+        self.song_length = length
+
+    def name(self):
+        return self.song_name
+
+    def length(self):
+        return self.song_length
